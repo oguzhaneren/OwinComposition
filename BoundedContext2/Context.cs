5520b8878b5c4c7c6f4707763953792d46b15776
Oguzhan Eren <oguzh4n@gmail.com>
2016-07-29 15:07:00 +0300

    - initial commit

---
+++ BoundedContext2/Context.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using SimpleInjector;
+
+namespace BoundedContext2
+{
+    public class Context
+    {
+        public void HandleRequests(Func<IOwinContext, Task> handler)
+        {
+            
+        }
+
+        public void RegisterApp(IAppBuilder appBuilder)
+        {
+            var container = new Container();
+
+            appBuilder.Use((context, next) =>
+            {
+                return next();
+            });
+
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            appBuilder.UseWebApi(config);
+
+            container.RegisterWebApiControllers(config, Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ OwinCompositionHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace OwinCompositionHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var context1 = new BoundedContext1.Context();
+            var context2 = new BoundedContext2.Context();
+
+
+            using (WebApp.Start("http://localhost:8001", app =>
+            {
+                app.Map("/context1", appBuilder => context1.RegisterApp(appBuilder));
+                app.Map("/context2", appBuilder => context2.RegisterApp(appBuilder));
+            }))
+            {
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
